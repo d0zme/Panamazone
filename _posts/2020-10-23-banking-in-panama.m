79143343109493134618c45b7b8a4f6a7e306eec
d0zme <65038326+d0zme@users.noreply.github.com>
2020-10-23 08:26:44 -0400

    Create 2020-10-23-banking-in-panama.m

---
+++ _posts/2020-10-23-banking-in-panama.m
@@ -0,0 +1,41 @@
+---
+title: Banking in Panama
+layout: post
+permalink: /banking-in-panama
+image: https://i.imgur.com/R87kmhA.jpg
+---
+
+Banks in Panama have a reputation for secrecy, but that is mostly a thing of the past. International press makes us all believe this country is a financial paradise and the financial sector is indeed an important part of the local economy. However, this should not lead one to believe that one can easily open a bank account in Panama and expect the same quality of service as in North America or Western Europe.
+
+Opening an account as a newcomer can turn out to be a time and nerve consuming adventure. Several months of waiting are common if you have no roots set in this country. In general, you will be served in a very friendly manner when you go to the bank branch, but you should not expect everything to be handled unbureaucratically and quickly once you have left the branch. Especially if you leave the country for a while after that, you should not expect to find a usable account when you return. Inquiries by email are often not answered. Phone calls are difficult for newcomers who still speak little Spanish, especially since the English spoken by many bank employees is often difficult to understand. There is usually no other choice than to make the pilgrimage to the bank again and again after returning home, so that at some point the account is ready.
+
+Not all banks have online banking, but the ones that do treat it as a privilege. Procedures common in Europe, such as online transfers or standing orders, are not so straightforward, often only to customers of the same bank or certain institutional clients. I once had to pay a service provider who had his account at another bank. The procedure of registering his account with my bank for a transfer from my account was so cumbersome and time-consuming that I preferred to withdraw cash and deposit it into the account of the service provider's bank. The usual comfort of simply entering BIC and IBAN of any intra-European account on an online bank transfer has to be forgotten here. And the same applies to standing orders or even direct debit authorizations. Apart from the fact that it is best not to give direct debit authorizations in Panama, they can easily lead to sleepless nights and stomach problems.
+
+Current accounts with a checkbook and debit card do not give any active interest and require a relatively high minimum coverage, below which penalty fees are due. Savings accounts that can also be accessed by debit card have low active interest rates and require a (smaller) minimum balance to be maintained.
+
+Getting a loan, e.g. for building a house, is also at least a time-consuming and nerve-wracking adventure, regardless of the advertising that many banks make for it. I have personally experienced it that several of the banks addressed were not willing to accept the (much higher) value of my building plot as security for a building loan. They would have been willing to finance the purchase of a house, but I wanted to build a house, not buy a finished one, which would have been much more expensive and on top of that would have extremely limited my options, especially since I had already bought the land. One of the banks I approached was quite flexible, but had a very expensive fee structure and high interest rates. Last but not least, I had found a bank that was willing to finance the construction and charged relatively affordable interest and closing fees, but the paperwork would have taken several months (including the tax returns of the last three years, certified by the Panamanian Embassy of the country). I had already had my experience with the "speed" of Central American embassies and consulates, especially when one cannot go there directly and personally remind them of the completion of the application, so that in the end I decided not to borrow money from a Panamanian bank. Others may have different experiences, may have different requirements, but my personal experience in this respect was very negative.
+
+In the following, I will give some hints from members of an English speaking Panamanian forum about online banking.
+
+I have found a rather hard, but in principle probably accurate experience elsewhere:
+
+"While many institutions here are inefficient, the banks take the prize by a large margin.  I have several horror stories I could relate but for fear of an injuria y calumnia suit. ....  I have found the bank employees to be unfailingly polite (and the police, too, for that matter) but to be utterly feckless when it comes to results. If I can ever get out of a situation with a bank which has consumed my time and energy for over a year, has cost me thousands of dollars, and has left me debilitated emotionally and physically, I intend to live in Panama without the "benefits" of Panamanian banking."
+
+**The horror story of a bank client,**
+
+"While many institutions here are inefficient, the banks take the prize by a large margin.  I have several horror stories I could relate but for fear of an injuria y calumnia suit.  ....  I have found the bank employees to be unfailingly polite (and the police, too, for that matter) but to be utterly feckless when it comes to results. If I can ever get out of a situation with a bank which has consumed my time and energy for over a year, has cost me thousands of dollars, and has left me debilitated emotionally and physically, I intend to live in Panama without the "benefits" of Panamanian banking."
+
+Unfortunately, none of the banks seems to be optimal, each has advantages and disadvantages. In addition, the location of the branches and the ATMs will be taken into consideration when making the selection. Withdrawing money from ATMs is only free of charge if it is done at your own bank.
+
+The fortunate part about having a bank account in Panama would be the **ease of paying bills without having to stand in line at a grocery store**. And with Nequi being hooked up with Paypal, receiving money abroad is now quite easy (if you have the right bank account).
+
+## Opening A Bank Account In Panama
+
+Don't be afraid to try to open a bank account as a foreigner in Panama if you absolutely need it. All you need is patience to stand in line and the following documents:
+- Your passport
+- Driver’s license or some form of national ID.
+- One or two bank references
+- A statement of your account in good standing from your bank
+- Utility bill
+- Personal recommendation from someone in Panama
+- Most banks require a minimum deposit anywhere from US$50 to US$200, but some may ask more depending on your situation.
